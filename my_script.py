c702e4e14662bd8ac9ffd42905863eaa4f753c46
Adil Kamal Mir <aadilmir@gmail.com>
2019-10-12 18:33:50 -0400

    2-updates
    Signed-off-by: Adil Kamal Mir <aadilmir@gmail.com>

--- my_script.py
+++ my_script.py
@@ -10,15 +10,19 @@ def convert(s):
         except ValueError:
             return s
 
-
+# assuming if only first row is unique, they are headers
 def check_for_headers (input_list):
-    count = 0
-    for item in input_list[0]:
-        if type(item) == str:
-            count += 1
-    if count != len(input_list[0]):
+    headers = 0
+    first_row = [type(item) for item in input_list[0]]
+    for cur_row in input_list:
+        cur_row = [type(item) for item in cur_row]
+        if cur_row == first_row:
+            headers += 1
+    if headers == 1:
+        return input_list
+    else:
         input_list.insert(0, list(range(len(input_list[0]))))
-    return input_list
+        return input_list
 
 
 input_file = (sys.argv[1])
@@ -43,23 +47,3 @@ output_dict = {list_of_col_names[idx]:item for idx, item in enumerate(output_dic
 with open(output_file, 'w') as file_out:
     file_out.write("output_list: %s\r\n output_dict: %s\r\n" % (output_list, output_dict))
     file_out.close()
-
-# name_file = "housing.names"
-# names = open(name_file, 'r')
-# count = 0
-# for line in names:
-#     if line.find("Attribute Information") > -1 or line.find("Attribute information") > -1:
-#         next(names)
-#         for item in names:
-#             # print (item.split())
-#             if item.split() == [] and count > 0:
-#                 break
-#             elif item.split() == []:
-#                 count += 1
-#                 continue
-#             elif item.split() == [] and count > 0:
-#                 break
-#             elif item.split()[0].find(".") > -1:
-#                 print(item.split()[1])
-#             elif item.split()[0].find(")") > -1:
-#                 print(item.split()[1])
